47e2c49b5d33536df9b2e2b8067fea59042de6b7
Akash <parmar.aakash26@gmail.com>
2025-06-23 18:41:05 +0530

    Variables and Datatypes

---
+++ 01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 332211
+let accountEmail = "skr@gmail.com"
+var accountPassword = "0123456789"
+accountCity = "Ahmedabad"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+//******  Theories   ******* //
+//The var keyword was used in all JavaScript code from 1995 to 2015.
+//The let and const keywords were added to JavaScript in 2015.
+//The var keyword should only be used in code written for older browsers.
+//JavaScript identifiers are case-sensitive.
+
+/*
+Note: var : not to use var bcoz of issue in block scope({}) and functional scope
+*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+/* --- Data Types in Js ---- */
+
+/*  JavaScript has 8 Datatypes
+
+--> String : ""
+, Number: 123456789
+, Bigint:  for big numbers 
+, Boolean: ture/ false
+, Undefined: 
+, Null: stand alone value 
+, Symbol: unique component
+, Object:
+--> The Object Datatype : contain both built-in objects, and user defined objects
+* Built-in object types : objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+*/
+
+"use strict"; //treat all JS code as newer version 
+
+//alert(3 +3) // we are using nodejs, not brower
