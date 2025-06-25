728d96fd9e6171cece21a7be5eb015e2f9b2a102
Akash <parmar.aakash26@gmail.com>
2025-06-25 15:55:06 +0530

    Conversion Operations

--- 03_ConversionOperations.js
+++ 03_ConversionOperations.js
@@ -2,18 +2,45 @@
 
 // Datatypes always represents as first latter is capital 
 
-let score = "33"
+// let score = "33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 // now convertion  string to numbers 
 
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
 
 // "33" => 33
 // "33abc" => NaN (Not a number)
 // true => 1 , false => 0
 // Inidentified => NaN
 // " "  =>  false, "value" => True 
+
+
+/*    --- Conversion Operations --- */
+
+
+// Nagative value print 
+//let age = 32
+//let negValue = value
+//console.log(negValue);
+
+//all arithmetic operation so  practices yourself 
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2**2);
+
+
+// STRING CONVERSION 
+let str1 = "Hello"
+let str2 = " AKASH"
+let str3 = str1 + str2 
+console.log(str3)
+console.log(1 + "2")
+console.log("1" + 3)
+console.log("1" + "2")
+
