9740370aa15310ec27692adfab55c0556e168404
Akash <parmar.aakash26@gmail.com>
2025-06-26 20:47:11 +0530

    Stack & Heap memory

---
+++ 05_Stack_Heap-Memory.js
@@ -0,0 +1,8 @@
+/* Stack & Heap Memory */
+
+// Stack : all primitive type data (they give copy ) 
+
+// Heap : all non-primitive data (they give refrence)
+
+// video link  for understanding : https://www.youtube.com/watch?v=7gwc-1czolw&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=10
+
